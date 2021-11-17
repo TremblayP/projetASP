97a45f8354dc09e2d2e89e1bf0c769a1c3f5d4bd
TremblayP <Tremblay.pascal99@gmail.com>
2021-11-16 20:19:10 -0500

    envoyerMessage.aspx.cs termine

--- prjWinCsLavalifeFinal/envoyerMessage.aspx.cs
+++ prjWinCsLavalifeFinal/envoyerMessage.aspx.cs
@@ -18,39 +18,78 @@ namespace prjWinCsLavalifeFinal
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            myCon.Open();
-            if (Session["idPourM"] != null) {
-                string id = Session["idPourM"].ToString();
-
-                SqlCommand myCmd = new SqlCommand("SELECT * FROM users WHERE Id = @id", myCon);
-                myCmd.Parameters.AddWithValue("id", id);
-                SqlDataReader myRd = myCmd.ExecuteReader();
-                if (myRd.Read())
-                {
-                    lblClient.Text = myRd["fname"] + " " + myRd["fname"];
-
-                }
-            }
-    
-            else
-            {
-                lblClient.Visible = false;
-                cboClients.Visible = true;
-                string id = Session["userId"].ToString();
-                SqlCommand myCmd = new SqlCommand("SELECT * FROM users WHERE Id != @id", myCon);
-                myCmd.Parameters.AddWithValue("id", id);
-                SqlDataReader myRd = myCmd.ExecuteReader();
-                while (myRd.Read())
-                {
-                    ListItem dest = new ListItem();
-                    dest.Text = myRd["fname"].ToString() + " " + myRd["lname"].ToString() ;
-                    dest.Value = myRd["Id"].ToString();
-                    cboClients.Items.Add(dest);
  
+            if (!Page.IsPostBack)
+            {
+                mySet = getDataSet();
+                myCon.Open();
+                tabUser = mySet.Tables["Users"];
+                tabSpecifications = mySet.Tables["Specifications"];
+                tabMessages = mySet.Tables["Messages"];
+
+                if (Session["idPourM"] != null)
+                {
+                    string id = Session["idPourM"].ToString();
+                    /*
+                    SqlCommand myCmd = new SqlCommand("SELECT * FROM users WHERE Id = @id", myCon);
+                    myCmd.Parameters.AddWithValue("id", id);
+                    SqlDataReader myRd = myCmd.ExecuteReader();
+                    if (myRd.Read())
+                    {
+                        lblClient.Text = myRd["fname"] + " " + myRd["fname"];
+
+                    }*///avec dataset ici
+                    var users = from DataRow dr in tabUser.Rows
+                                where dr.Field<int>("Id").ToString() == id
+                                select new
+                                {
+                                    fname = dr.Field<string>("fname").ToString(),
+                                    lname = dr.Field<string>("lname").ToString()
+
+                                };
+                    foreach(var u in users)
+                    {
+                        lblClient.Text = u.fname + " " + u.lname;
+                    }
                 }
 
+                else
+                {
+                    lblClient.Visible = false;
+                    cboClients.Visible = true;
+                    string id = Session["userId"].ToString();
+                    /*
+                    SqlCommand myCmd = new SqlCommand("SELECT * FROM users WHERE Id != @id", myCon);
+                    myCmd.Parameters.AddWithValue("id", id);
+                    SqlDataReader myRd = myCmd.ExecuteReader();
+                    while (myRd.Read())
+                    {
+                        ListItem dest = new ListItem();
+                        dest.Text = myRd["fname"].ToString() + " " + myRd["lname"].ToString();
+                        dest.Value = myRd["Id"].ToString();
+                        cboClients.Items.Add(dest);
+
+                    }*/
+                    //avec dataset ici
+                    var usersForList = from DataRow dr in tabUser.Rows
+                                       where dr.Field<int>("Id").ToString() != id
+                                       select new
+                                       {
+                                           fname = dr.Field<string>("fname").ToString(),
+                                           lname = dr.Field<string>("lname").ToString(),
+                                           id = dr.Field<int>("Id").ToString()
+                                       };
+                    foreach(var v in usersForList)
+                    {
+                        ListItem dest = new ListItem();
+                        dest.Text = v.fname + " " + v.lname;
+                        dest.Value = v.id;
+                        cboClients.Items.Add(dest);
+                    }
+                    myCon.Close();
+                }
             }
-            myCon.Close();
+
 
         }
 
@@ -107,7 +146,7 @@ namespace prjWinCsLavalifeFinal
             String Titre = txtTitre.Text.Trim();
             String Mess = txtMessage.Text.Trim();
 
-
+            /*
             string sql = "INSERT INTO Messages(titre, message, envoyeur, receveur, dateCreation, nouveau)VALUES(@titre, @mess, @env, @rec, @date, 'true')";
             myCon.Open();
             SqlCommand mycmd = new SqlCommand(sql, myCon);
@@ -116,9 +155,27 @@ namespace prjWinCsLavalifeFinal
             mycmd.Parameters.AddWithValue("env", Session["userId"]);
             mycmd.Parameters.AddWithValue("rec", refDestin);
             mycmd.Parameters.AddWithValue("date", DateTime.Today);
+            
 
+            mycmd.ExecuteNonQuery();*/
+            //version dataset ici
+            DataRow myrow = tabMessages.NewRow();
+            myrow["titre"] = Titre.ToString();
+            myrow["message"] = Mess.ToString();
+            myrow["envoyeur"] = Session["userId"];
+            myrow["receveur"] = refDestin;
+            myrow["dateCreation"] = DateTime.Today;
+            myrow["nouveau"] = "true";
+            tabMessages.Rows.Add(myrow);
+            //synchro
+            SqlCommandBuilder myBuilder = new SqlCommandBuilder(adpMessages);
+            adpMessages.Update(mySet, "Messages");
+            //refill dataset
+            tabMessages = mySet.Tables["Messages"];
+            mySet.Tables.Remove(tabMessages);
+            adpMessages.Fill(mySet, "Messages");
+            tabMessages = mySet.Tables["Messages"];
 
-            mycmd.ExecuteNonQuery();
             myCon.Close();
             Response.Redirect("accueilMembre.aspx");
         }
